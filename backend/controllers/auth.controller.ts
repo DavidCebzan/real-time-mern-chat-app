a5a388db333169d1f8f3d90fcca1bde354e20578
DavidCebzan <david.cebzan@alianza.com>
2024-04-04 11:55:39 +0200

    add logout function

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -95,6 +95,13 @@ export const login = async (req: Request, res: Response) => {
     }
 }
 
-export const logout = (req: Request, res: Response) => {
-    console.log('Logot')
+export const logout = async (req: Request, res: Response) => {
+    try {
+        res.cookie('jwt', "", {maxAge: 0});
+
+        res.status(200).json({message: 'Looged out successfully'});
+    } catch (error) {
+        console.log('Error in login controller', JSON.stringify(error));
+        res.status(500).json({error: 'Internal Server Error'});
+    }
 }
