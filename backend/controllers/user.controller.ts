e4f7e31e995ec494b33a589b32fa13f3d310e0d4
DavidCebzan <david.cebzan@alianza.com>
2024-04-29 10:43:12 +0200

    add the user controler

--- backend/controllers/user.controller.ts
+++ backend/controllers/user.controller.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from "express";
+import { IUser } from "../types/db";
+import User from "../models/user.model";
 
-export const getAllUsers = async (req: Request, res: Response) => {
-    try {
-        
+interface CustomRequest extends Request {
+    user?: IUser;
+}
+
+export const getAllUsers = async (req: CustomRequest, res: Response) => {
+    try {    
+       const logedInUserId = req.user?._id;
+
+        // get all user but not our own user
+        // if we want all users then delete the object inside find function
+        const allUsers = await User.find({ _id: {$ne: logedInUserId}}).select('-password');
+
+        res.status(200).json(allUsers);
     } catch (error) {
-        console.log('Error in getMessages controller', JSON.stringify(error));
+        console.log('Error in getAllUsers controller', JSON.stringify(error));
         res.status(500).json({error: 'Internal Server Error'});
     }
 }
